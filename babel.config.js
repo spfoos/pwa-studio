78a56ef826a5ee7e399d199605285a0ba18c4892
zetlen <jzetlen@adobe.com>
2019-06-28 07:20:07 -0500

    fixup CI babel caching

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,10 @@
 module.exports = api => {
-    if (process.env.CODEBUILD_BUILD_ID) {
-        api.cache.never();
-    } else {
-        api.cache.using(() => process.env.NODE_ENV);
-    }
     // const env = api.env() || 'development';
     const config = { presets: ['@magento/peregrine'] };
-    // Ignore everything with underscores except stories in dev mode
-    if (api.env() === 'development') {
+    if (process.env.CODEBUILD_BUILD_ID) {
+        api.cache.never();
+    } else if (api.env() === 'development') {
+        // Ignore everything with underscores except stories in dev mode
         config.exclude = [/\/__(tests?|mocks|fixtures|helpers|dist)__\//];
     }
     return config;
